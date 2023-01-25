224075e01f301079330a74053a83fa16fc5a9fe1
Meryem Moujniba <94840600+Moujmeryem@users.noreply.github.com>
2023-01-25 22:29:14 +0100

    frst dp

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = () => ( <
+    footer className = 'footer' >
+    <
+    a href = 'https://www.linkedin.com/in/meryem-moujniba-6791381a5/'
+    className = 'link footer__link' >
+    be in touch <
+    /a> <
+    /footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,67 @@
+const header = {
+
+    homepage: 'https://www.linkedin.com/in/meryem-moujniba-6791381a5/',
+    title: 'Portfolio',
+}
+
+const about = {
+    name: 'Meryem Moujniba',
+    role: 'a Future Engineer',
+    description: 'Im student in SupMTI , its my first project in React js',
+    social: {
+        linkedin: 'https://www.linkedin.com/in/meryem-moujniba-6791381a5/',
+        github: 'https://github.com/Moujmeryem',
+    },
+}
+
+const projects = [
+
+    {
+        name: ' Portfolio ',
+        description: 'this page',
+        stack: ['React js'],
+        sourceCode: 'https://github.com/Moujmeryem',
+        livePreview: 'https://github.com/Moujmeryem',
+    },
+    {
+        name: 'yummy food',
+        description: 'Food mobile app',
+        stack: ['Android Studio'],
+        sourceCode: 'https://github.com/Moujmeryem',
+        livePreview: 'https://github.com/Moujmeryem',
+    },
+    {
+        name: 'Pet Shop',
+        description: '',
+        stack: ['jee'],
+        sourceCode: 'https://github.com/Moujmeryem',
+        livePreview: 'https://github.com/Moujmeryem',
+    },
+    {
+        name: 'Symfony API',
+        description: 'api app',
+        stack: ['Symfony'],
+        sourceCode: 'https://github.com/Moujmeryem',
+        livePreview: 'https://github.com/Moujmeryem',
+    },
+]
+
+const skills = [
+
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React js',
+    'Symfony',
+    'JAVA',
+    'Python',
+    'Android studio',
+
+]
+
+const contact = {
+
+    email: 'Moujnibameryem@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
